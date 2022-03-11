7a286dd2fb1b84a75a1b81df2ad5bff6d578cafc
shivasati123 <68387790+shivasati123@users.noreply.github.com>
2022-03-11 17:47:04 +0530

    wip/project-setup

---
+++ BackendCodes/src/main/java/com/signuplogin/backend/BackendCodesApplication.java
@@ -0,0 +1,13 @@
+package com.signuplogin.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCodesApplication.class, args);
+	}
+
+}
---
+++ BackendCodes/src/test/java/com/signuplogin/backend/BackendCodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signuplogin.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
